bae08418b2cbdc9000adc35d66aa3423900633c3
woosley <redicaps@gmail.com>
2011-07-08 15:52:03 +0800

    update doumail

--- lib/Net/Douban/Doumail.pm
+++ lib/Net/Douban/Doumail.pm
@@ -3,84 +3,143 @@ package Net::Douban::Doumail;
 use Moose;
 use MooseX::StrictConstructor;
 use Carp qw/carp croak/;
-with 'Net::Douban::Roles::More';
+with 'Net::Douban::Roles';
+use namespace::autoclean;
 
-has 'doumailID' => (
-    is  => 'rw',
-    isa => 'Str',
+our %api_hash = (
+    get_mail_inbox => {
+        path   => '/doumail/inbox',
+        method => 'GET',
+    },
+
+    get_mail_unread => {
+        path   => '/doumail/inbox/unread',
+        method => 'GET',
+    },
+
+    get_mail_outbox => {
+        path   => '/doumail/outbox',
+        method => 'GET',
+    },
+
+    get_mail => {
+        path          => '/doumail/{doumailID}',
+        has_url_param => 1,
+        method        => 'GET',
+    },
+
+    post_mail => {
+        path           => '/doumails',
+        method         => 'POST',
+        content_params => ['title', 'content', 'receiver'],
+        content        => <<'EOF',
+PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiPz4gPGVudHJ5IHhtbG5zPSJodHRw
+Oi8vd3d3LnczLm9yZy8yMDA1L0F0b20iIHhtbG5zOmRiPSJodHRwOi8vd3d3LmRvdWJhbi5jb20v
+eG1sbnMvIiB4bWxuczpnZD0iaHR0cDovL3NjaGVtYXMuZ29vZ2xlLmNvbS9nLzIwMDUiIHhtbG5z
+Om9wZW5zZWFyY2g9Imh0dHA6Ly9hOS5jb20vLS9zcGVjL29wZW5zZWFyY2hyc3MvMS4wLyI+IDxk
+YjplbnRpdHkgbmFtZT0icmVjZWl2ZXIiPiA8dXJpPmh0dHA6Ly9hcGkuZG91YmFuLmNvbS9wZW9w
+bGUve3JlY2VpdmVyfTwvdXJpPiA8L2RiOmVudGl0eT4gPGNvbnRlbnQ+e2NvbnRlbnR9PC9jb250
+ZW50PiA8dGl0bGU+e3RpdGxlfTwvdGl0bGU+IHtjYXB0Y2hhX3Rva2VufXtjYXB0Y2hhfSA8L2Vu
+dHJ5Pgo=
+EOF
+        _build_content => \&_check_captcha,
+
+    },
+
+    mark_mail_as_read => {
+        path          => '/doumail/{doumailID}',
+        has_url_param => 1,
+        method        => 'POST',
+        content       => <<'EOF',
+PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiPz4gPGVudHJ5IHhtbG5zPSJodHRw
+Oi8vd3d3LnczLm9yZy8yMDA1L0F0b20iIHhtbG5zOmRiPSJodHRwOi8vd3d3LmRvdWJhbi5jb20v
+eG1sbnMvIiB4bWxuczpnZD0iaHR0cDovL3NjaGVtYXMuZ29vZ2xlLmNvbS9nLzIwMDUiIHhtbG5z
+Om9wZW5zZWFyY2g9Imh0dHA6Ly9hOS5jb20vLS9zcGVjL29wZW5zZWFyY2hyc3MvMS4wLyI+IDxk
+YjphdHRyaWJ1dGUgbmFtZT0idW5yZWFkIj5mYWxzZTwvZGI6YXR0cmlidXRlPiA8L2VudHJ5Pgo=
+EOF
+    },
+
+    mark_mails_as_read => {
+        path    => '/doumail/',
+        method  => 'PUT',
+        content => <<'EOF',
+PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiPz4gPGZlZWQgeG1sbnM9Imh0dHA6
+Ly93d3cudzMub3JnLzIwMDUvQXRvbSIgeG1sbnM6ZGI9Imh0dHA6Ly93d3cuZG91YmFuLmNvbS94
+bWxucy8iIHhtbG5zOmdkPSJodHRwOi8vc2NoZW1hcy5nb29nbGUuY29tL2cvMjAwNSIgeG1sbnM6
+b3BlbnNlYXJjaD0iaHR0cDovL2E5LmNvbS8tL3NwZWMvb3BlbnNlYXJjaHJzcy8xLjAvIj4ge2Vu
+dHJpZXN9IDwvZmVlZD4K
+EOF
+        _build_content => \&check_mailID,
+    },
+
+    delete_mail => {
+        path          => '/doumail/{doumailID}',
+        has_url_param => 1,
+        method        => 'DELETE',
+    },
+
+    delete_mails => {
+        path          => '/doumail/delete',
+        has_url_param => 1,
+        method        => 'POST',
+        content       => <<'EOF',
+PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0iVVRGLTgiPz4gPGZlZWQgeG1sbnM9Imh0dHA6
+Ly93d3cudzMub3JnLzIwMDUvQXRvbSIgeG1sbnM6ZGI9Imh0dHA6Ly93d3cuZG91YmFuLmNvbS94
+bWxucy8iIHhtbG5zOmdkPSJodHRwOi8vc2NoZW1hcy5nb29nbGUuY29tL2cvMjAwNSIgeG1sbnM6
+b3BlbnNlYXJjaD0iaHR0cDovL2E5LmNvbS8tL3NwZWMvb3BlbnNlYXJjaHJzcy8xLjAvIj4ge2Vu
+dHJpZXN9IDwvZmVlZD4K
+EOF
+        _build_content => \&_delete_mails,
+
+    },
 );
 
-has 'doumail_url' => (
-    is      => 'rw',
-    isa     => 'Url',
-    lazy    => 1,
-    default => sub { shift->base_url . '/doumail' },
-);
-
-before qw/inbox unread outbox get_doumail/ => sub {
-    croak "oauth needed" unless $_[0]->has_oauth;
-};
-
-sub inbox {
-    my ($self, %args) = @_;
-    return Net::Douban::Atom->new(
-        $self->get($self->doumail_url . "/inbox", %args));
+sub _check_captcha {
+    my ($content, $args) = @_;
+    if (!exists $args->{captcha} && !exists $args->{captcha_token}) {
+        $content =~ s/{captcha}//g;
+        $content =~ s/{captcha_token}//g;
+    } elsif (exists $args->{captcha} && exists $args->{captcha_token}) {
+        $content =~ s/{captcha}/$args->{captcah}/g;
+        $content =~ s/{captcha_token}/$args->{captcha_token}/g;
+    } else {
+        croak "Missing augument: captcha_token/captcha";
+    }
+    return $content;
 }
 
-#sub unread {
-#    my ($self, %args) = @_;
-#    return Net::Douban::Atom->new(
-#        $self->get($self->doumail_url . "/inbox/unread", %args));
-#}
-#
-#sub outbox {
-#    my ($self, %args) = @_;
-#    return Net::Douban::Atom->new(
-#        $self->get($self->doumail_url . "/outbox", %args));
-#}
-#
-#sub get_doumail {
-#    my ($self, %args) = @_;
-#    $args{mailID} ||= $self->mailID;
-#    croak "mailID needed" unless defined $args{mailID};
-#    return Net::Douban::Atom->new(
-#        $self->get($self->doumail_url . "/$args{mailID}", %args));
-#}
-#
-#sub post_doumail {
-#    my ($self, %args) = @_;
-#    croak "post xml needed!" unless $args{xml};
-#    return $self->post($self->doumail_url . 's', xml => $args{xml});
-#}
-#
-#sub delete_doumail {
-#    my ($self, %args) = @_;
-#    $args{mailID} ||= $self->mailID;
-#    croak "mailID needed" unless defined $args{mailID};
-#    return $self->delete($self->doumail_url . "/$args{mailID}");
-#}
-#
-#sub mark_read {
-#    my ($self, %args) = @_;
-#    $args{mailID} ||= $self->mailID;
-#    croak "mailID needed" unless defined $args{mailID};
-#    croak "put xml needed!" unless $args{xml};
-#    return $self->put($self->doumail_url . "/$args{mailID}",
-#        xml => $args{xml});
-#}
-#
-#sub delete {
-#    my ($self, %args) = @_;
-#    croak "delete xml needed!" unless $args{xml};
-#    return $self->delete($self->doumail_url, $args{xml});
-#}
-#
-#sub mark {
-#    my ($self, %args) = @_;
-#    croak "post xml needed!" unless $args{xml};
-#    return $self->put($self->doumail_url . '/delete', $args{xml},);
-#}
+sub _check_mailIDs {
+    my ($content, $args) = @_;
+    croak 'Missing augument: mailIDs' unless exists $args->{mailIDs};
+    my @mailIDs =
+      ref $args->{mailIDs} ? @{$args->{mailIDs}} : $args->{mailIDs};
+    my $entries;
+    for my $mail (@mailIDs) {
+        $entries .= <<"EOF";
+<entry> <id>http://api.douban.com/doumail/$mail</id> <db:attribute name="unread">false</db:attribute></entry>
+EOF
+    }
+    $content =~ s/{entries}/$entries/;
+    return $content;
+}
 
+sub _delete_mails {
+    my ($content, $args) = @_;
+    croak 'Missing augument: mailIDs' unless exists $args->{mailIDs};
+    my @mailIDs =
+      ref $args->{mailIDs} ? @{$args->{mailIDs}} : $args->{mailIDs};
+    my $entries;
+    for my $mail (@mailIDs) {
+        $entries .= <<"EOF";
+<entry> <id>http://api.douban.com/doumail/$mail</id>
+EOF
+    }
+    $content =~ s/{entries}/$entries/;
+    return $content;
+
+}
+
+__PACKAGE__->_build_method(%api_hash);
 __PACKAGE__->meta->make_immutable;
 1;
 
---
+++ t/13-doumail.t
@@ -0,0 +1,38 @@
+use lib './t/lib';
+use Test::Douban;
+use Test::More tests => 7;
+use Test::Exception;
+
+BEGIN {
+    use_ok("Net::Douban::Doumail");
+}
+
+my $mail = Net::Douban::Doumail->new();
+isa_ok($mail, 'Net::Douban::Doumail');
+my %api_hash = %{Net::Douban::Doumail::api_hash};
+
+cmp_ok(scalar keys %api_hash, ">", 0, "api_hash defined");
+
+can_ok($mail, keys %api_hash);
+
+SKIP: {
+    skip 'set $ENV{NETWORK_TEST} to enable network tests', 3
+      unless $ENV{NETWORK_TEST};
+    $mail->res_callback(sub {shift});
+    $mail->load_token(%{pdakeys()});
+
+    is($mail->get_mail_inbox->is_success,
+        1, "get inbox mail ok");
+    is($mail->get_mail_outbox->is_success,
+        1, "get mail outbox");
+    is($mail->get_mail(doumailID=> '55556547')->is_success,
+        1, "get doumail");
+#   is($mail->post_mail(
+#                receiver => 'Net-Douban',
+#                content => 'test net-douban ' . localtime(time),
+#                title => 'helo',
+#                )->is_success, 1, 'post mail');
+#    $mail->clear_res_callback;
+#    my $entries = $mail->get_mail_unread->{entry};
+#    cmp_ok(scalar @{$entries}, '>', 0, "get unread mail");
+}
